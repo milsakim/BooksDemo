3503c249d9336f6a6aae82498b80a7fd30edb2df
milsakim <hyejeekim@me.com>
2022-08-29 15:00:51 +0900

    feat: BookListViewModel 내의 불필요한 메소드 삭제 fetchBookList(of:)는 pagination 및 이미 fetch 중인지에 대한 플래그 처리가 되어 있지 않아 data race 위험이 존재했음. currentPage를 fetch 중인지 알려주는 isFetchingPage 플래그와 fetchBooksIfNeeded(currentBook:), fetchMoreBooks(with:)의 도입으로 fetchBookList(of:)의 역할 및 data race 위험성을 제거함.

--- BooksDemo/Model/ViewModel/BookListViewModel.swift
+++ BooksDemo/Model/ViewModel/BookListViewModel.swift
@@ -29,25 +29,6 @@ class BookListViewModel: ObservableObject {
     deinit {
         print("--- BookListViewModel deinit ---")
     }
-
-    // books는 main queue에서 업데이트 되기 때문에 동시다발적으로 books에 접근하는 것은 방지가 됨.
-    // 다만 data race는 발생할 수 있음.
-    // 어차피 pagination에 대한 처리가 필요해서 이 부분은 보완이 필요함.
-    func fetchBookList(of keyword: String) {
-        let requestModel: RequestModel = RequestModel(endPoint: BookStoreSearchEndPoint(searchKeyword: keyword, page: currentPage))
-        
-        bookSearchRequestable.request(requestModel) { result in
-            switch result {
-            case .success(let response):
-                DispatchQueue.main.async {
-                    self.books = response.books
-                }
-                print("--- response: \(response) ---")
-            case .failure(let error):
-                print("--- error: \(error) ---")
-            }
-        }
-    }
     
     func fetchBooksIfNeeded(currentBook book: Book?) {
         guard let book = book, let searchKeyword = searchKeyword else {
