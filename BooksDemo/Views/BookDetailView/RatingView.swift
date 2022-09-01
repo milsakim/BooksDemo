869e4756a764fa1dc14014fa5b87e2da46b7dc62
milsakim <hyejeekim@me.com>
2022-09-02 00:36:19 +0900

    feat: RatingView 구현

--- BooksDemo/Views/BookDetailView/RatingView.swift
+++ BooksDemo/Views/BookDetailView/RatingView.swift
@@ -12,11 +12,46 @@ struct RatingView: View {
     
     var body: some View {
         HStack {
-            Image(systemName: "star.fill")
-            Image(systemName: "star.fill")
-            Image(systemName: "star.fill")
-            Image(systemName: "star.fill")
-            Image(systemName: "star.fill")
+            switch rating {
+            case 0..<1:
+                Image(systemName: "star")
+                Image(systemName: "star")
+                Image(systemName: "star")
+                Image(systemName: "star")
+                Image(systemName: "star")
+            case 1..<2:
+                Image(systemName: "star.fill")
+                Image(systemName: "star")
+                Image(systemName: "star")
+                Image(systemName: "star")
+                Image(systemName: "star")
+            case 2..<3:
+                Image(systemName: "star.fill")
+                Image(systemName: "star.fill")
+                Image(systemName: "star")
+                Image(systemName: "star")
+                Image(systemName: "star")
+            case 3..<4:
+                Image(systemName: "star.fill")
+                Image(systemName: "star.fill")
+                Image(systemName: "star.fill")
+                Image(systemName: "star")
+                Image(systemName: "star")
+            case 4..<5:
+                Image(systemName: "star.fill")
+                Image(systemName: "star.fill")
+                Image(systemName: "star.fill")
+                Image(systemName: "star.fill")
+                Image(systemName: "star")
+            case 5:
+                Image(systemName: "star.fill")
+                Image(systemName: "star.fill")
+                Image(systemName: "star.fill")
+                Image(systemName: "star.fill")
+                Image(systemName: "star.fill")
+            default:
+                Text("Unkown Rating")
+            }
         }
     }
 }
