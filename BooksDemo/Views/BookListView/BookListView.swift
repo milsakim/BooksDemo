52540b7a8dceb2073f1ae05faf2273afae25cfc6
milsakim <hyejeekim@me.com>
2022-09-07 09:16:02 +0900

    feat: ScrollView 내부 컨텐츠 사이즈가 작아질 시 함께 줄어드는 현상 수정

--- BooksDemo/Views/BookListView/BookListView.swift
+++ BooksDemo/Views/BookListView/BookListView.swift
@@ -12,38 +12,45 @@ struct BookListView: View {
     @State private var searchKeyword: String = ""
     
     var body: some View {
-        ScrollView {
-            if !bookListViewModel.books.isEmpty {
-                LazyVStack(alignment: .leading) {
-                    ForEach(bookListViewModel.books, id: \.isbn13) { book in
-                        NavigationLink(destination: BookDetailView(isbn: book.isbn13)) {
-                            BookListRowView(title: book.title, urlString: book.image)
+        GeometryReader { proxy in
+            ScrollView {
+                if !bookListViewModel.books.isEmpty {
+                    LazyVStack(alignment: .leading) {
+                        ForEach(bookListViewModel.books, id: \.isbn13) { book in
+                            NavigationLink {
+                                BookDetailView(isbn: book.isbn13)
+                            } label: {
+                                BookListRowView(title: book.title, urlString: book.image)
+                            }
                         }
+                        
+                        Color.clear
+                            .onAppear {
+                                print("--- clear view appeared ---")
+                                bookListViewModel.fetchMoreBooks(with: searchKeyword)
+                            }
                     }
-                    
-                    Color.clear
-                        .onAppear {
-                            print("--- clear view appeared ---")
-                            bookListViewModel.fetchMoreBooks(with: searchKeyword)
-                        }
+                }
+                else if bookListViewModel.books.isEmpty && !bookListViewModel.isFetchingPage {
+                    Text("No Search Result")
+                        .frame(width: proxy.size.width)
+                }
+                
+                if bookListViewModel.isFetchingPage {
+                    ProgressView()
+                        .frame(width: proxy.size.width)
                 }
             }
-            else if bookListViewModel.books.isEmpty && !bookListViewModel.isFetchingPage {
-                Text("No Search Result")
+            .frame(width: proxy.size.width)
+            .searchable(text: $searchKeyword)
+            .onChange(of: searchKeyword) { keyword in
+                print("--- keyword on change: \(keyword) \(searchKeyword)---")
+                bookListViewModel.setSearchKeyword(to: keyword)
             }
-            
-            if bookListViewModel.isFetchingPage {
-                ProgressView()
+            .onSubmit(of: .search) {
+                print("--- submit!!!!!! ---")
+                bookListViewModel.setSearchKeyword(to: searchKeyword)
             }
-        }
-        .searchable(text: $searchKeyword)
-        .onChange(of: searchKeyword) { keyword in
-            print("--- keyword on change: \(keyword) \(searchKeyword)---")
-            bookListViewModel.setSearchKeyword(to: keyword)
-        }
-        .onSubmit(of: .search) {
-            print("--- submit!!!!!! ---")
-            bookListViewModel.setSearchKeyword(to: searchKeyword)
         }
     }
 }
